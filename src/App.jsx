67c2e9d87077c159aa5a20a98a81b12dfb2e036f
Aryan-A1 <aryankumarsingh9370@gmail.com>
2025-03-11 11:37:52 +0530

    Complete work

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,16 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+import {Route, Routes} from 'react-router-dom'
 import './App.css';
 import Usercard from './components/usercard.jsx';
 
+
 function App() {
-  const cards = Array.from({ length: 10 });
-  const gridStyle = {
-    display: 'grid',
-    gridTemplateColumns: 'repeat(4, 1fr)',
-    gap: '16px',
-    padding: '16px',
-  };
 
   return (
-    <div style={gridStyle}>
-      {cards.map((_, index) => (
-        <Usercard key={index} />
-      ))}
-    </div>
+    <>
+    <Routes>
+      <Route path='/' element={<Usercard/>}/>
+    </Routes>
+    </>
   );
 }
 
--- src/components/usercard.jsx
+++ src/components/usercard.jsx
@@ -1,1 +1,29 @@
 // write the component code here
+// write the component code here
+import React from 'react'
+import './usercard.css'
+
+
+export default function Usercard() {
+
+  const pfp = "https://i.pinimg.com/736x/24/84/70/248470199d7901dd9f5adbed7a6a3932.jpg" 
+  const name = "Aryan"
+  const email = "aryankumarsingh9370@gmail.com"
+  const phone = "8785xxxxx"
+  const add = "Sector 124, House No. 64, Mohali"
+  return (
+    <div className='container'>
+
+      <div className='card'>
+
+      <img src={pfp} alt="Profile photo" className='pfp'/>
+      <h2> Name : {name} </h2>
+      <p > <b>Email :</b> <a href="mailto:{email}">{email}</a> </p>
+      <p> <b>Phone number :</b> {phone} </p>
+      <p> <b>Address :</b> {add} </p>
+
+      </div>
+
+    </div>
+  )
+}
